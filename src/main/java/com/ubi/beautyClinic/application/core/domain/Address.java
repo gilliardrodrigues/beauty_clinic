e0585541480d1878c9b77321c38679ac17fd847a
Gilliard Gabriel Rodrigues <gilliard2019@ufmg.br>
2023-05-22 14:27:33 +0100

    <feat>: jakarta validation constraints added to PhoneNumber and Address.

--- src/main/java/com/ubi/beautyClinic/application/core/domain/Address.java
+++ src/main/java/com/ubi/beautyClinic/application/core/domain/Address.java
@@ -1,8 +1,10 @@
 package com.ubi.beautyClinic.application.core.domain;
 
-import lombok.*;
-
-import javax.persistence.Embeddable;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
@@ -10,10 +12,21 @@ import javax.persistence.Embeddable;
 @NoArgsConstructor
 public class Address {
 
+    @NotBlank(message = "O campo 'street' é obrigatório.")
     private String street;
-    private String number;
+
+    @NotBlank(message = "O campo 'houseNumber' é obrigatório.")
+    private String houseNumber;
+
+    @NotBlank(message = "O campo 'neighborhood' é obrigatório.")
     private String neighborhood;
+
+    @NotBlank(message = "O campo 'city' é obrigatório.")
     private String city;
+
+    @NotBlank(message = "O campo 'state' é obrigatório.")
     private String state;
+
+    @NotBlank(message = "O campo 'country' é obrigatório.")
     private String country;
 }
--- src/main/java/com/ubi/beautyClinic/application/core/domain/PhoneNumber.java
+++ src/main/java/com/ubi/beautyClinic/application/core/domain/PhoneNumber.java
@@ -1,10 +1,11 @@
 package com.ubi.beautyClinic.application.core.domain;
 
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Embeddable;
 
 @Embeddable
 @Data
@@ -12,7 +13,14 @@ import javax.persistence.Embeddable;
 @NoArgsConstructor
 public class PhoneNumber {
 
+    @NotBlank(message = "O campo 'countryCode' é obrigatório.")
+    @Pattern(regexp = "\\+[0-9]+", message = "O campo 'countryCode' deve estar no formato '+123'.")
     private String countryCode;
+
+    @Pattern(regexp = "[0-9]*", message = "O campo 'areaCode' deve conter apenas números.")
     private String areaCode;
+
+    @NotBlank(message = "O campo 'number' é obrigatório.")
+    @Pattern(regexp = "[0-9]+", message = "O campo 'number' deve ser numérico.")
     private String number;
 }
