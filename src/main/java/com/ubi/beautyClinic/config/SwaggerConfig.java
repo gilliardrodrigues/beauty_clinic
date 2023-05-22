158e88ef31a9a06e0229c8643b2d103e025d2204
Gilliard Gabriel Rodrigues <gilliard2019@ufmg.br>
2023-05-22 14:41:56 +0100

    <feat>: swagger ui initial configurations added.

---
+++ src/main/java/com/ubi/beautyClinic/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.ubi.beautyClinic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
+                .resourceChain(false);
+    }
+}
